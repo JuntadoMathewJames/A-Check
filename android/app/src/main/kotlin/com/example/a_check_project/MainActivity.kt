0c4243b413dbe1c4d3f0c70d2af911200aef47cd
James Mathew Juntado <jmjuntado@gbox.adnu.edu.ph>
2023-05-07 16:21:19 +0800

    first push

---
+++ android/app/src/main/kotlin/com/example/a_check_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a_check_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/dashboard.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class MyDashBoard extends StatelessWidget {
+  const MyDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.shade400,
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(foregroundColor: Colors.black),
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.edit,
+                ),
+                label: const Text('Edit Class',
+                    style: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Align(
+              alignment: Alignment.topRight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("ITMC101\t\t\tZT12"),
+                      Text("INTRODUCTION TO COMPUTING",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15)),
+                      Text("Room: P217", style: TextStyle(color: Colors.black54)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black),
+                        onPressed: () {
+                          print("you pressed generate code");
+                        },
+                        child: const Text("Generate Code"),
+                      ),
+                      const SizedBox(height: 4),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black),
+                        onPressed: () {
+                          print("you pressed assign beadle");
+                        },
+                        child: const Text("Assign Beadle"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
