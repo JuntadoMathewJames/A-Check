652fa30399b86b35100554f851a0110147e98c85
ikukee <rmpanis2@gmail.com>
2023-05-11 02:09:34 +0800

    change home:

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:a_check_project/firebase_options.dart';
+import 'package:a_check_project/widgets/sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -6,8 +7,7 @@ import 'package:flutter/material.dart';
 import 'widgets/classDashboard.dart';
 import 'widgets/sign_up.dart';
 
-
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: SignUp(),
+      home: Login(),
     );
   }
 }
