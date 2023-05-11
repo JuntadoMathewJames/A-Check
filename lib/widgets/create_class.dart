e0c53f75b0ba00b1a13cf72f7f5832dd26fb0be7
James Mathew Juntado <jmjuntado@gbox.adnu.edu.ph>
2023-05-11 14:56:39 +0800

    fixed overflowed pixels

--- lib/widgets/create_class.dart
+++ lib/widgets/create_class.dart
@@ -29,87 +29,92 @@ class CreateClass extends StatelessWidget {
           title: const Text('CREATE CLASS'),
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              const Padding(padding: EdgeInsets.all(5)),
-              const SizedBox(
-                child: Text("Create a Class",
-                    textAlign: TextAlign.justify,
-                    style:
+          child: ListView(
+            children:[
+              Column(
+                children: <Widget>[
+                  const Padding(padding: EdgeInsets.all(5)),
+                  const SizedBox(
+                    child: Text("Create a Class",
+                        textAlign: TextAlign.justify,
+                        style:
                         TextStyle(fontWeight: FontWeight.w400, fontSize: 40)),
-              ),
-              const Padding(padding: EdgeInsets.all(5)),
-              SizedBox(
-                width: 500,
-                child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Class Name",
-                        hintText: "eg. English")),
-              ),
-              const Padding(padding: EdgeInsets.all(5)),
-              SizedBox(
-                width: 500,
-                child: TextField(
-                    controller: classController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Class Code",
-                        hintText: "eg. ENG101")),
-              ),
-              const Padding(padding: EdgeInsets.all(5)),
-              SizedBox(
-                width: 500,
-                child: TextField(
-                    controller: sectionController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Class Section",
-                        hintText: "eg. ZC23")),
-              ),
-              const Padding(padding: EdgeInsets.all(5)),
-              Container(
-                width: 500,
-                height: 200,
-                decoration: BoxDecoration(border: Border.all(width: 1)),
-              ),
-              const Padding(padding: EdgeInsets.all(5)),
-              Container(
-                  width: 500,
-                  alignment: Alignment.bottomRight,
-                  child: SizedBox(
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Class Name",
+                            hintText: "eg. English")),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                        controller: classController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Class Code",
+                            hintText: "eg. ENG101")),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  SizedBox(
+                    width: 500,
+                    child: TextField(
+                        controller: sectionController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Class Section",
+                            hintText: "eg. ZC23")),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  Container(
+                    width: 500,
+                    height: 200,
+                    decoration: BoxDecoration(border: Border.all(width: 1)),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  Container(
+                    width: 500,
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
                       width: 250,
                       height: 50,
                       child: TextButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  const MaterialStatePropertyAll<Color>(
-                                      Colors.black),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      side: const BorderSide(
-                                          color: Colors.black)))),
-                          child: const Text(
-                            'Add Class',
-                            style: TextStyle(color: Colors.white, fontSize: 23),
-                          ),
-                          onPressed: () {
-                            final name = nameController.text;
-                            final section = sectionController.text;
-                            final classcode = classController.text;
+                        style: ButtonStyle(
+                            backgroundColor:
+                            const MaterialStatePropertyAll<Color>(
+                                Colors.black),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    side: const BorderSide(
+                                        color: Colors.black)))),
+                        child: const Text(
+                          'Add Class',
+                          style: TextStyle(color: Colors.white, fontSize: 23),
+                        ),
+                        onPressed: () {
+                          final name = nameController.text;
+                          final section = sectionController.text;
+                          final classcode = classController.text;
 
-                            createClass(
-                                name: name,
-                                section: section,
-                                classcode: classcode);
-                          },
+                          createClass(
+                              name: name,
+                              section: section,
+                              classcode: classcode);
+                        },
                       ),
+                    ),
                   ),
+                ],
               ),
-        ],
+
+            ],
           ),
         ),
 
