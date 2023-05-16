afec8b6694a67f1d2f59aa0fb297c4ede4a23260
ikukee <rmpanis2@gmail.com>
2023-05-16 21:52:44 +0800

    trying

--- lib/widgets/mainDashboard.dart
+++ lib/widgets/mainDashboard.dart
@@ -1,55 +1,40 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart'; //created by google
 import 'sideBar.dart';
 import 'circleButton.dart';
 import 'bottomBar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // ignore: use_key_in_widget_constructors
 class MainDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final CollectionReference classes =
-        FirebaseFirestore.instance.collection('classes');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
       drawer: const MySideBar(),
-      body: Center(
-          child: Stack(
-        children: <Widget>[
-          Column(children: [
-            StreamBuilder(
-                stream: classes.snapshots(),
-                builder:
-                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-                  if (streamSnapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: streamSnapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          final DocumentSnapshot documentSnapshot =
-                              streamSnapshot.data!.docs[index];
-                          return Card(
-                            margin: const EdgeInsets.all(10),
-                            child: ListTile(
-                                title: Text(documentSnapshot['name']),
-                                subtitle: Text(documentSnapshot['section'])),
-                          );
-                        });
-                  } else {
-                    return Text("no data");
-                  }
-                })
-          ]),
-          const Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: EdgeInsets.all(42.0),
-              child: CircleButton(),
-            ),
+      body: Row(children: [
+        StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('classes').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) =>
+                        Text(snapshot.data!.docs[index]['name']));
+              } else {
+                return Container();
+              }
+            }),
+        const Align(
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: EdgeInsets.all(42.0),
+            child: CircleButton(),
           ),
-        ],
-      )),
+        ),
+      ]),
       bottomNavigationBar: const BottomBreadcrumbBar(),
     );
   }
